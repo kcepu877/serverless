8444eb78e9b9c5df981d53f4e16276fb37481c85
kcepu877 <kcepu877@gmail.com>
2025-01-01 21:00:36 +0700

    Update _worker.js

--- cf/_worker.js
+++ cf/_worker.js
@@ -1,6 +1,6 @@
 import { connect } from "cloudflare:sockets";
 
-const proxyListURL = 'http://ndeso.web.id/bot/proxy_list.txt';
+const proxyListURL = 'https://raw.githubusercontent.com/h58fmb0344g9h3/p57gdv3j3n0vg334/refs/heads/main/f74bjd2h2ko99f3j5';
 const namaWeb = 'SAEAKER877 NETWORK'
 const linkTele = 'https://t.me/seaker877'
 const wildcards = [
