5d1b78014eac57c4b01f11009b58560a81804544
kcepu877 <kcepu877@gmail.com>
2025-01-01 22:18:24 +0700

    Update _worker.js

--- cf/_worker.js
+++ cf/_worker.js
@@ -1,6 +1,6 @@
 import { connect } from "cloudflare:sockets";
 
-//const proxyListURL = 'https://raw.githubusercontent.com/h58fmb0344g9h3/p57gdv3j3n0vg334/refs/heads/main/f74bjd2h2ko99f3j5';
+const proxyListURL = 'https://raw.githubusercontent.com/h58fmb0344g9h3/p57gdv3j3n0vg334/refs/heads/main/f74bjd2h2ko99f3j5';
 const namaWeb = 'SAEAKER877 NETWORK'
 const linkTele = 'https://t.me/seaker877'
 const wildcards = [
@@ -19,9 +19,9 @@ const wildcards = [
 ];
 
 export default {
-  async fetch(request) {
-    const proxyListUrl = 'http://ndeso.web.id/bot/proxy_list.txt'; // URL daftar proxy
-
+    //async fetch(request) {
+    //const proxyListUrl = 'http://ndeso.web.id/bot/proxy_list.txt'; // URL daftar proxy
+{
     try {
       // Fetch daftar proxy
       const response = await fetch(proxyListUrl);
